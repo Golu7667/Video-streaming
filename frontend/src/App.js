7c1706a7f2c3a100cefadb81285b2932dec1276e
unknown <goluchoudhary7808@gmail.com>
2023-09-01 13:30:43 +0530

    add backend url in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); 
+const socket = io('https://webrtc-cx7r.onrender.com'); 
 
 const VideoChat = () => {
   const localVideoRef = useRef();
