65e40f743f6572dd864ce1687651e9e1367d5a14
unknown <goluchoudhary7808@gmail.com>
2023-10-28 17:35:28 +0530

    usermodel deta save in lowercase

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,10 +4,17 @@ const mongoose=require("mongoose")
 const userSchema=mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true},
-    active:{type:Boolean,default: false}
 
 },
 { timestaps: true })
+
+userSchema.pre('save', function (next) {
+    this.name = this.name.toLowerCase();
+    this.email = this.email.toLowerCase()
+    next();
+  });
+
+
 
 const User=mongoose.model("User",userSchema)
 module.exports=User;
